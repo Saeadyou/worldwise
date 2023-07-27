c3de0d32a839d632a472bce9878dddb83e54666e
saeadyou <saeadyou@gmail.com>
2023-07-27 15:05:52 +0200

    use Context API

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -3,7 +3,10 @@ import Message from "./Message";
 import styles from "./CityList.module.css";
 import CityItem from "./CityItem";
 
-function CityList({ cities, isLoading }) {
+import { useCities } from "../contexts/CitiesContext";
+
+function CityList() {
+  const { cities, isLoading }=useCities()
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,49 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const CitiesContext = createContext();
+
+const BASE_URL = "http://localhost:8000";
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+
+        setCities(data);
+      } catch (err) {
+        alert("There was an error during loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        setCities,
+        isLoading,
+        setIsLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
